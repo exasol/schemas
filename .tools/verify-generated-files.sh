1a9ed7f4dc33f24eb7cfba7cac29c725e44a48ea
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-13 08:58:44 +0100

    Generate files before veryfing

--- .tools/verify-generated-files.sh
+++ .tools/verify-generated-files.sh
@@ -6,14 +6,24 @@ set -o pipefail
 
 project_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 
-cd "$project_dir"
-if [[ $(git status --porcelain) ]]; then
-  echo "🛑 Please generate files as described in doc/developers_guide.md"
-  echo "git status --no-pager"
-  git status
-  echo "git diff --no-pager"
-  git diff
-  exit 1
-else
-  echo "✅ All files up-to-date"
-fi
+generate_files() {
+    cd "$project_dir"
+    pandoc README.md > index.html
+}
+
+verify_no_changes() {
+    cd "$project_dir"
+    if [[ $(git status --porcelain) ]]; then
+      echo "🛑 Please generate files as described in doc/developers_guide.md"
+      echo "git status --no-pager"
+      git status
+      echo "git diff --no-pager"
+      git diff
+      exit 1
+    else
+      echo "✅ All files up-to-date"
+    fi
+}
+
+generate_files
+verify_no_changes
