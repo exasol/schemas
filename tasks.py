0c759e48a3b3045b2c0953fc78bc75d5db8537d4
Nicola Coretti <nicola.coretti@exasol.com>
2023-12-20 10:56:34 +0100

    Update publish to test task

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 import shutil
-from tempfile import TemporaryDirectory
-from invoke import task
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from invoke import task
 
 ROOT = Path(__file__).parent
 
@@ -18,7 +19,7 @@ def build(ctx):
     """Build the static site for the JSON schemas"""
     ctx.run(
         f"schema2html --schemas {ROOT / 'schemas'} --examples {ROOT / 'examples'} --destination {ROOT / 'gh-pages'}",
-        pty=True
+        pty=True,
     )
 
 
@@ -28,21 +29,22 @@ def publish_to_test(ctx, version=None):
     repo_name = "schemas"
     repo_url = f"git@github.com:exasol/{repo_name}.git"
 
-    version = ctx.run('git rev-parse --short HEAD', hide=True).stdout.strip()
+    version = ctx.run("git rev-parse --short HEAD", hide=True).stdout.strip()
     with TemporaryDirectory() as tmp_dir:
         tmp_dir = Path(tmp_dir)
         checkout_path = tmp_dir / repo_name
         git_dir = f"--git-dir={checkout_path / '.git'}"
         work_tree = f"--work-tree={checkout_path}/"
         ctx.run(f"git clone {repo_url} {checkout_path}")
-        ctx.run(f"git {git_dir} switch --orphan gh-pages-test")
-        build_dir = tmp_dir / 'gh-pages'
+        ctx.run(f'git {git_dir} config user.email "opensource@exasol.com"')
+        ctx.run(f'git {git_dir} config user.name "PublishBot"')
+        ctx.run(f"git {git_dir} {work_tree} switch --orphan gh-pages-test")
+        build_dir = tmp_dir / "gh-pages"
         ctx.run(
             f"schema2html --schemas {ROOT / 'schemas'} --examples {ROOT / 'examples'} --destination {build_dir}",
-            pty=True
+            pty=True,
         )
-        shutil.copytree(build_dir, checkout_path)
-        breakpoint()
+        shutil.copytree(build_dir, checkout_path, dirs_exist_ok=True)
         ctx.run(f"git {git_dir} {work_tree} add {checkout_path}/")
         ctx.run(f'git {git_dir} commit -m "Publish version: {version}"')
-        #ctx.run(f"git {git_dir} push -f origin gh-pages-test")
+        ctx.run(f"git {git_dir} push -f origin gh-pages-test")
