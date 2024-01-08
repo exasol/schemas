67565d3dd45b663e38ea5e0800e82ba5931d2e83
Nicola Coretti <nicola.coretti@exasol.com>
2024-01-08 09:35:20 +0100

    Replace bootprint json schema with json schema for humans (#26)
    --------
    Co-authored-by: Christoph Pirkl <christoph.pirkl@exasol.com>

---
+++ src/exasol/schemas/__init__.py
@@ -0,0 +1,177 @@
+from __future__ import annotations
+
+import contextlib
+import io
+import logging
+import shutil
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Type
+
+import jinja2
+import json_schema_for_humans as json2schema
+import typer
+from json_schema_for_humans import generate
+from json_schema_for_humans.generation_configuration import GenerationConfiguration
+from packaging.version import Version
+from rich.logging import RichHandler
+
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    handlers=[RichHandler(show_time=False, show_path=False)],
+)
+
+_logger = logging.getLogger("schema2html")
+_stderr = _logger.info
+
+
+@dataclass(frozen=True)
+class Schema:
+    name: str
+    version: str
+    path: Path
+
+    @property
+    def full_name(self):
+        return f"{self.name}-{self.version}"
+
+
+@dataclass(frozen=True)
+class Example:
+    name: str
+    version: str
+    path: Path
+
+    @property
+    def full_name(self):
+        return f"{self.name}-{self.version}-example"
+
+
+CLI = typer.Typer()
+
+
+@CLI.command()
+def generate_static_page(
+    schemas: Path = typer.Option(
+        help="The path to the directory containing JSON schemas."
+    ),
+    examples: Path = typer.Option(
+        help=(
+            "The path to the directory with example data instances. "
+            "These examples should conform to the JSON schemas provided and serve as reference implementations."
+        )
+    ),
+    destination: Path = typer.Option(
+        help="The target directory, to be created, where the generated static page contents will be written to."
+    ),
+):
+    """
+    This command generates a static website based on provided JSON schemas and their corresponding example instances.
+
+    The static website created includes the following components:
+
+        - Overview Page: A summary page that introduces the contents and purpose of the schemas.
+
+        - JSON Schemas: The raw json schemas (so they can be referenced).
+
+        - Examples: Instances of data conforming to the JSON schemas, providing practical examples of usage.
+
+        - Human-Readable References: Each individual schema is accompanied by an overview page presenting the schema in a user-friendly  format.
+    """
+    schemas = set(_json_files(schemas, output_type=Schema))
+    examples = set(_json_files(examples, output_type=Example))
+
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        _create_schemas(schemas, output_dir=tmpdir)
+        _create_examples(examples, output_dir=tmpdir)
+        _create_schema_references(schemas, output_dir=tmpdir)
+        _create_index_file(examples, schemas, output_dir=tmpdir)
+        _copy_to_output_directory(src=tmpdir, dst=destination)
+
+
+def _json_files(path: Path, output_type: Type[Schema] | Type[Example]):
+    files = path.glob("**/*.json")
+    return (
+        output_type(
+            name=f.parent.name, version=Version(f.name.removesuffix(".json")), path=f
+        )
+        for f in files
+    )
+
+
+def _jinja_env() -> jinja2.Environment:
+    env = jinja2.Environment(
+        loader=jinja2.PackageLoader("exasol.schemas", "templates"),
+        autoescape=jinja2.select_autoescape(),
+    )
+    return env
+
+
+def _grouped_schemas(schemas):
+    grouped = defaultdict(list)
+    for schema in schemas:
+        grouped[schema.name].append(schema)
+    return grouped
+
+
+def _grouped_examples(examples):
+    grouped = defaultdict(lambda: defaultdict(None))
+    for example in examples:
+        group = grouped[example.name]
+        group[example.version] = example
+    return grouped
+
+
+def _copy_to_output_directory(src, dst):
+    if dst.exists():
+        _logger.warning(f"Location not empty, deleting destination: {dst}")
+        shutil.rmtree(dst)
+    shutil.move(src, dst)
+
+
+def _create_index_file(examples, schemas, output_dir):
+    s = _grouped_schemas(schemas)
+    e = _grouped_examples(examples)
+    env = _jinja_env()
+    template = env.get_template("index.jinja")
+    with open(output_dir / "index.html", "w") as f:
+        print(template.render(schemas=s, examples=e), file=f)
+
+
+def _create_schema_references(schemas, output_dir):
+    for schema in schemas:
+        reference = output_dir / f"{schema.full_name}.html"
+
+        with contextlib.redirect_stdout(io.StringIO()) as f:
+            json2schema.generate.generate_from_filename(
+                schema_file_name=schema.path,
+                result_file_name=f"{reference}",
+                config=GenerationConfiguration(template_name="js_offline"),
+            )
+
+        lines = (line for line in f.getvalue().split("\n") if line)
+        lines = (line.replace("==", "").strip() for line in lines)
+        for line in lines:
+            _stderr(line)
+
+
+def _create_examples(examples, output_dir):
+    for example in examples:
+        src = example.path
+        dest = output_dir / f"{example.full_name}.json"
+        shutil.copyfile(src, dest)
+
+
+def _create_schemas(schemas, output_dir):
+    for schema in schemas:
+        src = schema.path
+        dest = output_dir / f"{schema.full_name}.json"
+        shutil.copyfile(src, dest)
+
+
+if __name__ == "__main__":
+    CLI()
---
+++ tasks.py
@@ -0,0 +1,50 @@
+import shutil
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from invoke import task
+
+ROOT = Path(__file__).parent
+
+
+@task
+def fmt(ctx):
+    """Apply code formatter to all python files in the workspace"""
+    ctx.run("ruff format", pty=True)
+    ctx.run("ruff check --fix --select I001 --select I002", pty=True)
+
+
+@task
+def build(ctx):
+    """Build the static site for the JSON schemas"""
+    ctx.run(
+        f"schema2html --schemas {ROOT / 'schemas'} --examples {ROOT / 'examples'} --destination {ROOT / 'gh-pages'}",
+        pty=True,
+    )
+
+
+@task
+def publish_to_test(ctx, version=None):
+    """Build and publishes static website of JSON schemas to test branch"""
+    repo_name = "schemas"
+    repo_url = f"git@github.com:exasol/{repo_name}.git"
+
+    version = ctx.run("git rev-parse --short HEAD", hide=True).stdout.strip()
+    with TemporaryDirectory() as tmp_dir:
+        tmp_dir = Path(tmp_dir)
+        checkout_path = tmp_dir / repo_name
+        git_dir = f"--git-dir={checkout_path / '.git'}"
+        work_tree = f"--work-tree={checkout_path}/"
+        ctx.run(f"git clone {repo_url} {checkout_path}")
+        ctx.run(f'git {git_dir} config user.email "opensource@exasol.com"')
+        ctx.run(f'git {git_dir} config user.name "PublishBot"')
+        ctx.run(f"git {git_dir} {work_tree} switch --orphan gh-pages-test")
+        build_dir = tmp_dir / "gh-pages"
+        ctx.run(
+            f"schema2html --schemas {ROOT / 'schemas'} --examples {ROOT / 'examples'} --destination {build_dir}",
+            pty=True,
+        )
+        shutil.copytree(build_dir, checkout_path, dirs_exist_ok=True)
+        ctx.run(f"git {git_dir} {work_tree} add {checkout_path}/")
+        ctx.run(f'git {git_dir} commit --message="Publish version: {version}"')
+        ctx.run(f"git {git_dir} push --force origin gh-pages-test")
