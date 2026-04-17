d6cd7fca7d5d8141af10de7718e40991176e2203
redcatbear <sebastian.baer@exasol.com>
2026-04-17 09:51:52 +0200

    #50: Moved argument parsing.

--- src/exasol/schemas/__main__.py
+++ src/exasol/schemas/__main__.py
@@ -1,6 +1,21 @@
-import typer
+import argparse
+from pathlib import Path
 
 from exasol.schemas import generate_static_page
 
+
+def _parse_args():
+    parser = argparse.ArgumentParser(prog="python -m exasol.schemas")
+    parser.add_argument("--schemas", type=Path, required=True)
+    parser.add_argument("--examples", type=Path, required=True)
+    parser.add_argument("--destination", type=Path, required=True)
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    typer.run(generate_static_page)
+    args = _parse_args()
+    generate_static_page(
+        schemas=args.schemas,
+        examples=args.examples,
+        destination=args.destination,
+    )
